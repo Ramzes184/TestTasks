f4c50c2f7e1c088d1b9605a30b80a0c221345564
Ramzes184 <91544179+Ramzes184@users.noreply.github.com>
2021-10-15 16:13:46 +0700

    Update Task1.kt

--- Task1.kt
+++ Task1.kt
@@ -1,13 +1,4 @@
 
-fun main(){
-    println(" Task1\n-------------------------------")
-    println("Введите слова через пробел : ")
-    val entered = readLine()
-    if(entered == null){
-        println(entered)
-    }
-    else{
-        println("Входные данные введены некорректно")
-    }
-    println("-------------------------------\n Task1 ended.")
+fun main(args : Array<String>){
+    args.forEach(::println)
 }
