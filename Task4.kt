b9896cb9edd104620b0496b313aad3fb597abdf2
Ramzes184 <91544179+Ramzes184@users.noreply.github.com>
2021-10-15 18:44:50 +0700

    Update Task4.kt

--- Task4.kt
+++ Task4.kt
@@ -1,24 +1,5 @@
-fun main(){
-    println(" Task4\n-------------------------------")
-    println("Введите слова через пробел : ")
-    val entered = readLine()
-    if(entered != null){
-        val sortedLine = entered.split(" ").sorted().toMutableList()
-        println(sortedLine)
-        val occurrences: MutableMap<String, Int> = HashMap()
-
-        for (word in sortedLine) {
-            var oldCount = occurrences[word]
-            if (oldCount == null) {
-                oldCount = 0
-            }
-            occurrences[word] = oldCount + 1
-        }
-        println(occurrences)
-
+fun main(args: Array<String>){
+    args.groupingBy { it }.eachCount().toSortedMap().forEach(){
+        println("${it.key} ${it.value}")
     }
-    else {
-        println("Входные данные введены некорректно")
-    }
-    println("-------------------------------\n Task4 ended.")
 }
