4960d71fc4998d49997cdd5f8f55493f929d5bf3
ivanpylaev <pilaev.ivan80@mail.ru>
2021-10-21 23:41:24 +0700

    # 1 / 8

--- src/Task6.kt
+++ src/Task6.kt
@@ -11,5 +11,5 @@ fun main(args: Array<String>){
 }
 
 fun show (array: List<String>){
-    array.sorted().groupingBy { it }.eachCount().toSortedMap().forEach(::println)
+    array.sorted().groupingBy { it }.eachCount().map { "${it.key} ${it.value}" }.forEach(::println)
 }
