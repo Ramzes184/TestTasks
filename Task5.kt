54dccf3a630c9c0923d88b9d30cee60a092d1cc6
ivanpylaev <pilaev.ivan80@mail.ru>
2021-10-21 22:55:34 +0700

    # 1 / 7

--- Task5.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>){
-    args.groupingBy { it }.eachCount().toSortedMap().toList().sortedByDescending{ it.second }.toMap().forEach(){
-        println("${it.key} ${it.value}")
-    }
-}
-
-
---
+++ src/Task5.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    args.groupingBy { it }.eachCount().toSortedMap().toList().sortedByDescending{ it.second }.toMap().forEach(){
+        println("${it.key} ${it.value}")
+    }
+}
+
+
