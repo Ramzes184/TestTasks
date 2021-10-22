18c08b9eb30b1ef580781ef80f4a005e5e983304
ivanpylaev <pilaev.ivan80@mail.ru>
2021-10-22 22:10:40 +0700

    # 1 / 9

--- BUILD.sh
+++ BUILD.sh
@@ -3,4 +3,4 @@ kotlinc out/artifacts/Tasks/task2.kt -include-runtime -d task2.jar
 kotlinc out/artifacts/Tasks/task3.kt -include-runtime -d task3.jar
 kotlinc out/artifacts/Tasks/task4.kt -include-runtime -d task4.jar
 kotlinc out/artifacts/Tasks/task5.kt -include-runtime -d task5.jar
-kotlinc out/artifacts/Tasks/task6.kt -include-runtime -d task5.jar
+kotlinc out/artifacts/Tasks/task6.kt -include-runtime -d task6.jar
